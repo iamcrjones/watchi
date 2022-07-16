1199ecbdde1400232ec9c61ec9768d68d2d93bb5
Cameron Jones <iamcrjones@gmail.com>
2022-07-16 10:57:23 +1000

    initial packages installed. Drawer and theme toggle added

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,34 @@
-import React from 'react'
-import About from './About.js'
+import React, { useState } from 'react'
+import { ThemeProvider } from 'styled-components';
+import {lightTheme, darkTheme, GlobalStyles } from './styled/Global.style.js';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import NightlightIcon from '@mui/icons-material/Nightlight';
+import NavDrawer from './Drawer.js';
 
 
 const App = () => {
-  return (
-    <div >
-      <h1>hello there.... ʘ‿ʘ</h1>
-      <About />
-    </div>
-  )
-}
+  const [theme, setTheme] = useState('light')
 
+  const themeToggle = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light')
+  }
+
+  const iconToggle = () => {
+    if(theme === 'light'){
+      return <NightlightIcon fontSize="large"/>
+    }else {
+      return <LightModeIcon fontSize="large"/>
+    }
+  }
+  return (
+    <>
+      <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+        <GlobalStyles />
+        <NavDrawer toggle={themeToggle} icon={iconToggle}/>
+        <h1>hello there.... ʘ‿ʘ</h1>
+        <h4>Website is still under construction 🛠</h4>
+      </ThemeProvider>
+    </>
+  )
+  }
 export default App
---
+++ src/components/styled/Global.style.js
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from "styled-components";
+
+const pallet= {
+    pallet1: "#2A99DE",
+    pallet2: "#52A3FF",
+    pallet3: "#3BC9F5"
+}
+
+export const lightTheme = {
+    body: "#fff",
+    color: "#000"
+}
+
+export const darkTheme = {
+    body: "#000",
+    color: "#fff"
+}
+
+const fonts = {
+    exo: "'Exo', sans-serif;",
+    bangers: "'Bangers', cursive;"
+
+}
+export const GlobalStyles = createGlobalStyle`
+
+    body {
+        background-color: ${(props) => props.theme.body};
+        color: ${(props) => props.theme.color};
+        font-family: ${fonts.exo};
+    }
+    h1 {
+        color: ${pallet.pallet1};
+    }
+`
