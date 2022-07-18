54abd4ad0d376e5acd37b3596342a97fba83970c
matildamort <matilda.jane.morton@gmail.com>
2022-07-19 09:57:18 +1000

    added SignIn and SignUp forms and About section

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,9 +1,26 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { Box } from '@mui/material';
+
+
 const SignIn = () => {
-    return(
-        <>
-            <p> hello </p>
-        </>
-    )
+    return (
+        <Box>
+            <Card>
+                <CardContent>
+                    <h1>Sign In</h1>
+                    <form>
+                        <label>Email:</label>
+                        <input type="text" name="Email" />
+                        <label>Password:</label>
+                        <input type="text" name="Password" />
+                        <input type="submit" value="Sign In" />
+                    </form>
+                </CardContent>
+            </Card>
+        </Box>
+    );
 }
 
 export default SignIn;
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,32 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { Box } from '@mui/material';
+
+
 const SignUp = () => {
-    return(
-        <>
-            <p> hello </p>
-        </>
-    )
+    return (
+        <Box>
+            <Card>
+                <CardContent>
+                    <h1>Sign In</h1>
+                    <form>
+                        <label>Email:</label>
+                        <input type="text" name="Email" />
+                        <label>First Name:</label>
+                        <input type="text" name="First Name" />
+                        <label>Last Name:</label>
+                        <input type="text" name="Last Name" />
+                        <label>Last Name:</label>
+                        <input type="text" name="Last Name" />
+                        <label>User Name:</label>
+                        <input type="User Name" name="User Name" />
+                        <input type="submit" value="Sign In" />
+                    </form>
+                </CardContent>
+            </Card>
+        </Box>
+    );
 }
 
 export default SignUp;
