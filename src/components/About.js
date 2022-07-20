ae703fdcbac71dbfc53a1af3358aa1946f2204f8
Cameron Jones <iamcrjones@gmail.com>
2022-07-20 18:51:03 +1000

    only 1 request made now

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,10 @@
 import * as React from 'react';
 import abouthero from '../images/Hero-image.png'
-import { getShows } from './services/showServices.js'
 
 
 
 
 const About = () => {
-    let showList = []
-    const shows = getShows()
-    .then(data => {
-        showList.push(data)
-        return showList
-    })
-    .catch(e=> {console.log(e)})
     return(
         <>
             <div className="aboutContainer">
@@ -21,9 +13,6 @@ const About = () => {
                     Some stuff about this page and why it's super Awesome
                 </p>
                 <img className="AboutPic" src={abouthero} alt="lots of different Anime characters merged in one"></img>
-                <div>
-                    {shows.data}
-                </div>
 
             </div>
         </>
