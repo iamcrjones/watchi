5bd37a533e65222545f10e789c5968d78a6d7e6a
matildamort <matilda.jane.morton@gmail.com>
2022-07-29 18:07:58 +1000

    Have Signin page mostly same as wireframes

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,14 +1,12 @@
 import * as React from 'react';
 import { Box } from '@mui/material';
-import charizard from '../images/Charazard-Gif.gif'
-import { useState } from "react"
-import {signIn} from './services/authServices'
+import charizard from '../images/Charazard-Gif.gif';
+import { useState } from "react";
+import {signIn} from './services/authServices';
 import { Button, TextField } from '@mui/material';
-import Link from '@mui/material/Link';
+// import Link from '@mui/material/Link'
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
-
-
 
 
 
@@ -19,7 +17,7 @@ const SignIn = () => {
     }
 
     const [formData, setFormData] = useState(initialFormData)
-    const [error, setError] = useState(null)
+    const setError = useState(null)
 
 
 
@@ -55,70 +53,69 @@ const SignIn = () => {
 
 
 
-return(
+    return(
 
-    <Container maxWidth="l">
-        
-    <Grid container spacing={2} sx={{ 
-        height: '100vh',
-        alignItems: 'center',
-        justifyContent: 'center',
-        }}>
-
-      <Grid item xs={6}>
-        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>  
-        <h1>Login</h1>
-        <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                onChange={handleFormData}
-              />
-
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                onChange={handleFormData}
-              />
-              <Grid container spacing={2} justify="flex-end">
-
-                <Grid item >
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 3, mb: 2 }}
-                    >
-                        Sign In
-                    </Button>
-                    </Grid>
-
+        <Container maxWidth="l">
+            
+        <Grid container spacing={2} sx={{ 
+            height: '100vh',
+            alignItems: 'center',
+            justifyContent: 'center',
+            }}>
+    
+          <Grid item xs={6}>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>  
+            <h1>Login</h1>
+            <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                    onChange={handleFormData}
+                  />
+    
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    onChange={handleFormData}
+                  />
+                  <Grid container spacing={2} justify="flex-end">
+    
                     <Grid item >
-                        <p> Dont have an account? SignUp! </p>
-                    </Grid>
-              </Grid>
-        </Box>
-        
-    </Grid>
-    <Grid item xs={6}>
-    <img className="signUpPic" src={charizard} alt="dancing Pikachu"></img>
-        
-    </Grid>
-    </Grid>
-    </Container>
-  );
-}
-
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign In
+                        </Button>
+                        </Grid>
+    
+                        <Grid item >
+                            <p> Dont have an account? SignUp! </p>
+                        </Grid>
+                  </Grid>
+            </Box>
+            
+        </Grid>
+        <Grid item xs={6}>
+        <img className="signUpPic" src={charizard} alt="dancing Pikachu"></img>
+            
+        </Grid>
+        </Grid>
+        </Container>
+      );
+    }
 
 export default SignIn;
