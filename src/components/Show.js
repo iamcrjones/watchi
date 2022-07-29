3a0e4ede22f115d2d50e2d65bf89762448c7ab52
Cameron Jones <iamcrjones@gmail.com>
2022-07-29 12:53:29 +1000

    update

--- src/components/Show.js
+++ src/components/Show.js
@@ -1,49 +1,38 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import Box from '@mui/material/Box';
+// import Box from '@mui/material/Box';
 import { Link } from "react-router-dom"
 import { Typography } from '@mui/material';
-// import { useState, useEffect } from "react";
-// import axios from 'axios';
+import { getShows } from './services/showServices.js'
 
-
-    // useEffect((show) => {
-    //     let showList = []
-    //     getShows()
-    //     .then(data => {
-    //         showList.push(data)
-    //         return showList
-    //     })
-    //     .catch(e=> {console.log(e)})
-    //   }, [])
-
-
-    
-    const Shows = ({show}) => {
+    const Shows = () => {
+        getShows()
+        .then(data => {
+            return data
+        })
+        .catch(e=> {console.log(e)})
         return (
            <>
             <Card>
                 <CardContent>
-                        <link to={`/shows/${show.id}`}>
-                            <Typography variant="h5">{show.title}</Typography>
-                            </link>
-                            <Typography variant="body1">{show.description}</Typography>
-                </CardContent>    
+                        <Link to="/">
+                            <Typography variant="h5">{sessionStorage.getItem("shows")}</Typography>
+                            </Link>
+                            <Typography variant="body1">world</Typography>
+                </CardContent>
             </Card>
             </>
         )
     }
-    
-    
-{/* //     // const getRating = () => {
-//     //     getRating(rating)
-//     //     let total = 0;
-//     //     for(let i = 0; i < data.length; i++){
-//     //         total += data[i].rating;
-//     //     }
-//     //     let average = total / data.length;
-//     //     return average;
-//     //  */}
+// {/* //     // const getRating = () => {
+// //     //     getRating(rating)
+// //     //     let total = 0;
+// //     //     for(let i = 0; i < data.length; i++){
+// //     //         total += data[i].rating;
+// //     //     }
+// //     //     let average = total / data.length;
+// //     //     return average;
+// //     //  */}
 
 
 export default Shows;
