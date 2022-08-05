424039331ef24f16b1e686bdc6887d5505194fff
matildamort <matilda.jane.morton@gmail.com>
2022-08-05 14:55:59 +1000

    stuff

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -2,7 +2,12 @@
 import * as React from 'react';
 import { useState, useEffect} from 'react'
 import { getMyShows } from './services/watchlistServices';
-import { pullSingleShow } from './services/showServices';
+import Grid from '@mui/material/Grid'; 
+import Card from '@mui/material/Card';
+import Button from '@mui/material/Button';
+import { Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import RemoveShow from './RemoveShow'
 
 
 
@@ -36,8 +41,75 @@ const Watchlist = () => {
             {error && <h1>{error}</h1>}
             {loading === false && watchList.map((shows) =>
                 <>
-                    <h1>{shows.title}</h1>
-                    <img src={shows.picture_url} alt={shows.title}></img>
+
+                    <Card className="shows" key={shows.id} >
+                    <Grid container className="card">
+
+                    <Grid item xs={12} md={12}>
+                        <img src={shows.picture_url} alt={shows.title} />
+                    </Grid>
+
+                    <Grid item xs={12} md={12}>
+                        <Link to={`/show/${shows.id}`} onClick={() => {sessionStorage.setItem('currentShow', shows.id)}}>{<Typography variant="h5">{shows.title}</Typography>}</Link>
+                    
+                    </Grid>
+
+                    {/* <Grid item className="releaseDays" xs={12} md={12}>
+                        <p>Release days: </p>
+                            {shows.monday ? <Typography variant="p">Monday </Typography> : null}
+                            {shows.tuesday ? <Typography variant="p">Tuesday </Typography> : null}
+                            {shows.wednesday ? <Typography variant="p">Wednesday </Typography> : null}
+                            {shows.thursday ? <Typography variant="p">Thursday </Typography> : null}
+                            {shows.friday ? <Typography variant="p">Friday </Typography> : null}
+                            {shows.saturday ? <Typography variant="p">Saturday </Typography> : null}
+                            {shows.sunday ? <Typography variant="p">Sunday </Typography> : null}
+                    </Grid> */}
+
+                    <Grid item className="dates" xs={12} md={12} >
+                     <Typography variant="p">Start Date: {shows.airdate}</Typography>
+                    </Grid>
+                    
+
+                    <Grid item className="dates" xs={12} md={12}>
+                        <Typography variant="p">End Date: {shows.enddate}</Typography>
+                    </Grid>
+
+
+                    {/* <Grid item className="displayIcons">
+                    {show.attributes.crunchyroll ? 
+                    // <TaskAltIcon className="icons" />
+                            // <img className="showIcons" src={crunchyroll} alt="crunchyroll icon"></img>
+                        :null}
+
+                        {show.attributes.funimation ? 
+                        // <TaskAltIcon className="icons" />
+                        // <img className="showIcons" src={funimation} alt="funimation icon"></img>
+                        :null}
+
+                        {show.attributes.netflix ? 
+                        // <TaskAltIcon className="icons" />
+                        // <img className="showIcons" src={netflix} alt="netflix icon"></img>
+                        :null}
+                    </Grid> */}
+
+                    <Grid item xs={12}>
+                        {/* <Button
+                            className="buttons"
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                        >
+                            Add Show
+                        </Button> */}
+                        </Grid>
+
+                                
+                        {/* <RemoveShow id={shows.id}/>  */}
+
+                    </Grid>
+                    
+                
+                </Card>
                 </>
             )}
         </>
